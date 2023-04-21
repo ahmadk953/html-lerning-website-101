1ac0eb1e16cec402d57fe22fea8f7467c09eeb84
Ahmad <103906421+ahmadk953@users.noreply.github.com>
2023-04-20 22:53:28 -0400

    Updated lots of content. Fixed bugs, removed items, and overall made the website look and feel better.

---
+++ JavaScript/main.js
@@ -0,0 +1,31 @@
+// Start of ripple button code
+const RippleButton = document.querySelector(".ripple-button");
+
+function mousePositionToCustomProp(event, element) {
+  let posX = event.offsetX;
+  let posY = event.offsetY;
+
+  element.style.setProperty("--x", posX + "px");
+  element.style.setProperty("--y", posY + "px");
+}
+
+RippleButton.addEventListener("click", (e) => {
+  mousePositionToCustomProp(e, RippleButton);
+  RippleButton.classList.add("pulse");
+  RippleButton.addEventListener("animationend", () => {
+    RippleButton.classList.remove("pulse");
+  });
+});
+// End of ripple button code
+// Start of alert button code
+const AlertButton = document.querySelector(".alert-button");
+
+AlertButton.addEventListener("click", (e) => {
+  mousePositionToCustomProp(e, AlertButton);
+  AlertButton.classList.add("pulse");
+  AlertButton.addEventListener("animationend", () => {
+    AlertButton.classList.remove("pulse");
+  });
+  alert("This is an alert. Just like you asked.");
+});
+// End of alert button code
