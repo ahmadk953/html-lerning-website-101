7f941b4e1024ab4c641fa6308e7e8c307e39bc52
Ahmad Khan <chofman@outoforgedu.onmicrosoft.com>
2023-11-13 19:10:00 -0500

    Fixed things

--- server.js
+++ server.js
@@ -1,50 +1,52 @@
 // Load Node modules
-var express = require("express");
+let express = require("express");
 const ejs = require("ejs");
-var path = require("path");
+let path = require("path");
 const PORT = process.env.PORT || 8080;
-const validator = require('validator');
+const validator = require("validator");
 // Initialise Express
-var app = express();
+let app1 = express();
+//Remove the x-powered-by from the request headers
+app1.disable("x-powered-by");
 // Render static files
-app.use(express.static("public"));
+app1.use(express.static("public"));
 // Set the view engine to ejs
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app1.set("views", path.join(__dirname, "views"));
+app1.set("view engine", "ejs");
 // Port website will run on
-app.listen(PORT, () => {
+app1.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
 
 // Routes
-app.get("*", (req, res) => {
-  const currentPage = validator.blacklist(req.path, '<>"\'()&*#%$');
-  console.log('currentPage:', currentPage);
+app1.get("*", (req, res) => {
+  const currentPage = validator.blacklist(req.path, "<>\"'()&*#%$");
+  console.log("currentPage:", currentPage);
   res.render("pages" + currentPage, { currentPage });
 });
-app.get("/", function (req, res) {
+app1.get("/", function (req, res) {
   res.render("pages/index.ejs", { currentPage: "home" });
 });
-app.get("/404", function (req, res, next) {
+app1.get("/404", function (req, res, next) {
   // trigger a 404 since no other middleware
   // will match /404 after this one, and we're not
   // responding here
   next();
 });
-app.get("/403", function (req, res, next) {
+app1.get("/403", function (req, res, next) {
   // trigger a 403 error
-  var err = new Error("not allowed!");
+  const err = new Error("not allowed!");
   err.status = 403;
   next(err);
 });
 
-app.get("/500", function (req, res, next) {
+app1.get("/500", function (req, res, next) {
   // trigger a generic (500) error
   next(new Error("keyboard cat!"));
 });
 
 //Handle 404
-app.use(function (req, res, next) {
+app1.use(function (req, res, next) {
   res.status(404);
 
   res.format({
@@ -61,9 +63,9 @@ app.use(function (req, res, next) {
 });
 
 //Handle 500
-app.use(function (err, req, res, next) {
+app1.use(function (err, req, res, next) {
   // we may use properties of the error object
-  // here and next(err) appropriately, or if
+  // here and next(err) app1ropriately, or if
   // we possibly recovered from the error, simply next().
   res.status(err.status || 500);
   res.render("pages/404", { error: err, currentPage: "500" });
